fd999bfae4cd1be685fdeb04a99d6d521ca1dc08
Bram Wiepjes <bramw@protonmail.com>
2021-01-25 18:26:29 +0000

    Resolve "Add token usage details"

--- backend/src/baserow/contrib/database/tokens/handler.py
+++ backend/src/baserow/contrib/database/tokens/handler.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.utils import timezone
 
 from rest_framework.request import Request
 
@@ -422,3 +423,20 @@ class TokenHandler:
                                            'token.')
 
         token.delete()
+
+    def update_token_usage(self, token):
+        """
+        Increases the amount of handled calls and updates the last call timestamp of
+        the token.
+
+        :param token: The token instance that needs to be updated.
+        :param token: Token
+        :return: The updated token instance.
+        :rtype: Token
+        """
+
+        token.handled_calls += 1
+        token.last_call = timezone.now()
+        token.save()
+
+        return token
