d035109bcfce8b808e230ebe653777aff476a480
Bram Wiepjes <bramw@protonmail.com>
2021-05-31 00:01:31 +0200

    change root nginx config

--- deploy/heroku/entry.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-sed -i 's/$PORT/'"$PORT"'/g' /etc/nginx/sites-enabled/nginx.conf
-export BASEROW_PUBLIC_URL=${BASEROW_PUBLIC_URL:-https://$HEROKU_APP_NAME.herokuapp.com}
-/usr/bin/supervisord --configuration /etc/supervisor/conf.d/supervisor.conf
