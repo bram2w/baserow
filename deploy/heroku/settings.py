af5214b8f8fcd27e35e92b83242c7bbeaabe1264
Bram Wiepjes <bramw@protonmail.com>
2021-06-15 17:50:11 +0200

    concurrency

--- deploy/heroku/settings.py
+++ deploy/heroku/settings.py
@@ -27,10 +27,18 @@ if os.getenv("AWS_ACCESS_KEY_ID", "") != "":
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
     AWS_S3_OBJECT_PARAMETERS = {
         "CacheControl": "max-age=86400",
         "ContentDisposition": "attachment",
     }
     AWS_S3_FILE_OVERWRITE = True
     AWS_DEFAULT_ACL = "public-read"
+
+if os.getenv("AWS_S3_REGION_NAME", "") != "":
+    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+
+if os.getenv("AWS_S3_ENDPOINT_URL", "") != "":
+    AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
+
+if os.getenv("AWS_S3_CUSTOM_DOMAIN", "") != "":
+    AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_S3_CUSTOM_DOMAIN")
