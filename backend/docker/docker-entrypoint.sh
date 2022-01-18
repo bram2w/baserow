9896ae27ab1e91554f54705897e3d5516c3b61da
Nigel Gott <nigel@baserow.io>
2022-01-18 21:54:25 +0000

    Attempt to get coverage html another way

--- backend/docker/docker-entrypoint.sh
+++ backend/docker/docker-entrypoint.sh
@@ -110,10 +110,10 @@ case "$1" in
       make lint-python
     ;;
     ci-test)
-      mkdir reports/
+      mkdir reports/coverage_html/ -p
       coverage run -m pytest tests ../premium/backend/tests -n 2 --junitxml=reports/report.xml --html=reports/pytest_report.html --self-contained-html || exit;
       coverage xml -o reports/coverage.xml
-      coverage html -o reports/coverage.html
+      coverage html -d reports/coverage_html/
     ;;
     celery)
         exec celery -A baserow "${@:2}"
