2d1fd862844c720b22bf1565944b439ae9cb774d
Nigel Gott <nigel@baserow.io>
2022-01-20 16:09:46 +0000

    Attempt to fix failing nuxt server jobs by giving them different ports

--- web-frontend/test/helpers/create-nuxt.js
+++ web-frontend/test/helpers/create-nuxt.js
@@ -2,12 +2,12 @@ import { Builder, Nuxt } from 'nuxt'
 
 import config from '@/config/nuxt.config.test'
 
-export default async function createNuxt(buildAndListen = false) {
+export default async function createNuxt(buildAndListen = false, port = 3001) {
   const nuxt = new Nuxt(config)
   await nuxt.ready()
   if (buildAndListen) {
     await new Builder(nuxt).build()
-    await nuxt.server.listen(3001, 'localhost')
+    await nuxt.server.listen(port, 'localhost')
   }
   return nuxt
 }
