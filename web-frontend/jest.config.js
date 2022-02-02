7cfcb015a6e38d62d1b902158836d53f428857de
Nigel Gott <nigel@baserow.io>
2022-02-02 14:22:19 +0000

    Allow configuring the testApp with an optional setup function to be used by applications extending baserow.

--- web-frontend/jest.config.js
+++ web-frontend/jest.config.js
@@ -1,4 +1,9 @@
+// The main jest config file used to run all of our tests.
 module.exports = {
+  // The rootDir used by jest must be the root of the repository so the premium tests
+  // and frontend code are contained within jest's rootDir. This is because:
+  // - Jest cannot collect coverage for files outside of its rootDir
+  // - Jest struggles to run tests which are outside of its rootDir.
   rootDir: '..',
   roots: ['<rootDir>/web-frontend/', '<rootDir>/premium/web-frontend'],
   moduleDirectories: ['<rootDir>/web-frontend/node_modules/'],
