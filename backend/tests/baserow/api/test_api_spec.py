c8d08dc77c1037237dc85fbabeee05aec50bbabc
Petr Stribny <petr@stribny.name>
2022-02-21 10:39:17 +0100

    Add test to validate OpenAPI schema

---
+++ backend/tests/baserow/api/test_api_spec.py
@@ -0,0 +1,9 @@
+from django.shortcuts import reverse
+from openapi_spec_validator import openapi_v3_spec_validator
+
+
+def test_openapi_spec(api_client):
+    response = api_client.get(reverse("api:json_schema"))
+
+    # If no exception is raised the spec is valid
+    openapi_v3_spec_validator.validate(response.json())
