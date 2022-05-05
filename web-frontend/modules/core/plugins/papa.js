196ca310e59e70dc2fccf801b3715c0199c97b98
Bram Wiepjes <bramw@protonmail.com>
2022-05-05 18:13:55 +0000

    Fix papa depedency for SaaS

--- web-frontend/modules/core/plugins/papa.js
+++ web-frontend/modules/core/plugins/papa.js
@@ -1,9 +1,10 @@
 import Papa from 'papaparse'
 
-export default function () {
+export default function (context, inject) {
   Papa.parsePromise = function (file, config = {}) {
     return new Promise((resolve, reject) => {
       Papa.parse(file, { complete: resolve, error: reject, ...config })
     })
   }
+  inject('papa', Papa)
 }
