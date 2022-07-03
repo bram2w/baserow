e026e9b56fe56015fa10c76103ebe49b87fb012f
Davide Silvestri <davide@baserow.io>
2022-07-03 13:10:36 +0200

    fix migrations

--- backend/src/baserow/contrib/database/file_import/models.py
+++ backend/src/baserow/contrib/database/file_import/models.py
@@ -8,9 +8,16 @@ def file_import_directory_path(instance, filename):
     return f"user_{instance.user.id}/file_import/job__{instance.id}.json"
 
 
+# If you ever change the return value of this function please duplicate the old version
+# into migration database.0079_auto_20220704_1124 and change that migration to use the
+# duplicate to ensure this old migration doesn't logically change
+def report_default_value():
+    return {"failing_rows": {}}
+
+
 class FileImportJob(Job):
     table = models.ForeignKey(
         Table, on_delete=models.SET_NULL, null=True, related_name="import_jobs"
     )
     data_file = models.FileField(upload_to=file_import_directory_path, null=True)
-    report = models.JSONField(default=lambda: {"failing_rows": {}})
+    report = models.JSONField(default=report_default_value)
