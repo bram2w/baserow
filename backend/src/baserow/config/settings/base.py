a043fd5f8ab8e73c0b3ec11caac50559891237c6
Nigel Gott <nigel@baserow.io>
2022-09-07 13:24:00 +0000

    Bump for 1.12.0

--- backend/src/baserow/config/settings/base.py
+++ backend/src/baserow/config/settings/base.py
@@ -299,7 +299,7 @@ SPECTACULAR_SETTINGS = {
         "name": "MIT",
         "url": "https://gitlab.com/bramw/baserow/-/blob/master/LICENSE",
     },
-    "VERSION": "1.11.0",
+    "VERSION": "1.12.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "TAGS": [
         {"name": "Settings"},
