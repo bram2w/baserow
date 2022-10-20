745b0d3d6342c17563718fbfd2e9af26d1883aea
alex1431999 <alex@baserow.io>
2022-10-20 10:53:05 +0200

    Add missing docstrings

--- web-frontend/modules/database/membersPagePluginTypes.js
+++ web-frontend/modules/database/membersPagePluginTypes.js
@@ -1,22 +1,53 @@
 import { Registerable } from '@baserow/modules/core/registry'
 
 export class MembersPagePluginType extends Registerable {
+  /**
+   * Lets you manipulate the left columns of the members table to either add, remove
+   * or modify columns.
+   *
+   * You could always make sure to not make this function fail hard if it can't update
+   * or remove something, since other plugins might have also altered the columns.
+   */
   mutateMembersTableLeftColumns(leftColumns) {
     return leftColumns
   }
 
+  /**
+   * Lets you manipulate the right columns of the members table to either add, remove
+   * or modify columns.
+   *
+   * You could always make sure to not make this function fail hard if it can't update
+   * or remove something, since other plugins might have also altered the columns.
+   */
   mutateMembersTableRightColumns(rightColumns) {
     return rightColumns
   }
 
+  /**
+   * Lets you manipulate the right columns of the invites table to either add, remove
+   * or modify columns.
+   *
+   * You could always make sure to not make this function fail hard if it can't update
+   * or remove something, since other plugins might have also altered the columns.
+   */
   mutateMembersInvitesTableRightColumns(rightColumns) {
     return rightColumns
   }
 
+  /**
+   * Lets you manipulate the left columns of the invites table to either add, remove
+   * or modify columns.
+   *
+   * You could always make sure to not make this function fail hard if it can't update
+   * or remove something, since other plugins might have also altered the columns.
+   */
   mutateMembersInvitesTableLeftColumns(leftColumns) {
     return leftColumns
   }
 
+  /**
+   * Set to false in order to enable the plugin
+   */
   isDeactivated() {
     return true
   }
