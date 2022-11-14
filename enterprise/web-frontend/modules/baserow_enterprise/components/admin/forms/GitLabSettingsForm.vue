e572fe931d44c336ca49b67e6b317d7f293292bf
Davide Silvestri <davide@baserow.io>
2022-11-14 14:09:34 +0000

    Resolve "Remove "context_form" from SSO settings modals"

--- enterprise/web-frontend/modules/baserow_enterprise/components/admin/forms/GitLabSettingsForm.vue
+++ enterprise/web-frontend/modules/baserow_enterprise/components/admin/forms/GitLabSettingsForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="context__form" @submit.prevent="submit">
+  <form @submit.prevent="submit">
     <FormElement :error="fieldHasErrors('name')" class="control">
       <label class="control__label">{{
         $t('oauthSettingsForm.providerName')
