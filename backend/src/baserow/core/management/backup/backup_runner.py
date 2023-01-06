e0b717c30f8341de8653a91be6ff601e50572283
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-06 05:46:16 +0000

    Adding tarfile member sanitization to extractall()

--- backend/src/baserow/core/management/backup/backup_runner.py
+++ backend/src/baserow/core/management/backup/backup_runner.py
@@ -109,7 +109,29 @@ class BaserowBackupRunner:
 
         with tempfile.TemporaryDirectory() as temporary_directory_name:
             with tarfile.open(backup_file_name, "r:gz") as backup_input_tar:
-                backup_input_tar.extractall(temporary_directory_name)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(backup_input_tar, temporary_directory_name)
 
             backup_internal_folder_name = Path(backup_file_name).name
             backup_sub_folder = Path(
