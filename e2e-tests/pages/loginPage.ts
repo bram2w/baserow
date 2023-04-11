4efe8bd95957d4eba25aed0718ff271501859b8b
Petr Stribny <petr@stribny.name>
2023-04-11 17:44:53 +0200

    Remove screenshots

--- e2e-tests/pages/loginPage.ts
+++ e2e-tests/pages/loginPage.ts
@@ -19,7 +19,6 @@ export class LoginPage extends BaserowPage {
     await this.passwordInput.fill(password)
     const navigationPromise = this.page.waitForNavigation({timeout:5000})
     await this.loginButton.click()
-    await this.page.screenshot()
     await navigationPromise
   }
 }
