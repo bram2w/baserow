2ca124e80236d42d8cfa60e45da67abd6473a75d
Petr Stribny <petr@stribny.name>
2023-04-12 09:20:29 +0000

    Call ViewHandler().field_type_changed() even if the db field representation changes

--- backend/tests/baserow/contrib/database/field/test_field_handler.py
+++ backend/tests/baserow/contrib/database/field/test_field_handler.py
@@ -40,6 +40,7 @@ from baserow.contrib.database.fields.models import (
 from baserow.contrib.database.fields.registries import field_type_registry
 from baserow.contrib.database.rows.handler import RowHandler
 from baserow.contrib.database.table.models import Table
+from baserow.contrib.database.views.models import ViewFilter
 from baserow.core.exceptions import UserNotInWorkspace
 
 # You must add --run-disabled-in-ci to pytest to run this test, you can do this in
@@ -478,6 +479,43 @@ def test_update_field(send_mock, data_fixture):
         name=field_name_with_ok_length,
     )
     assert getattr(field_with_max_length_name, "name") == field_name_with_ok_length
+
+
+@pytest.mark.django_db
+@pytest.mark.field_formula
+@patch("baserow.contrib.database.fields.signals.field_updated.send")
+def test_update_field_reset_filter_formula_field(send_mock, data_fixture):
+    """
+    When there is a field change changing the underlying database
+    type of the field, the update_field() method should initiate
+    a cleanup for affected views. Formula fields can be in this
+    situation when the formula expression changes.
+    """
+
+    user = data_fixture.create_user()
+    table = data_fixture.create_database_table(user=user)
+    field = data_fixture.create_formula_field(table=table, order=1, formula="today()")
+    view = data_fixture.create_grid_view(table=table)
+    # create a view filter that won't be compatible with the new type
+    data_fixture.create_view_filter(
+        view=view, field=field, type="date_equals_today", value="UTC?"
+    )
+    model = table.get_model()
+    model.objects.create()
+    handler = FieldHandler()
+
+    field = handler.update_field(
+        user=user,
+        field=field,
+        new_type_name="formula",
+        formula="1",
+    )
+
+    field.refresh_from_db()
+    rows = model.objects.all()
+    assert getattr(rows[0], f"field_{field.id}") == Decimal("1")
+    # incompatible filter has been removed
+    assert ViewFilter.objects.filter(view=view).count() == 0
 
 
 # This failing field type triggers the CannotChangeFieldType error if a field is
