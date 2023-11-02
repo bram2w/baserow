ec18e4a28a59203ef3e2223edad3b19bf3dc4d0d
Bram Wiepjes <bramw@protonmail.com>
2023-11-02 15:21:10 +0000

    Resolve "Allow single select fields to have a radio box component instead of dropdown in form view"

---
+++ web-frontend/modules/database/components/view/form/FormViewFieldSingleSelectRadios.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="control__elements">
+    <div v-for="option in field.select_options" :key="option.id">
+      <Radio
+        :model-value="valueId"
+        :value="option.id"
+        @input=";[touch(), updateValue($event, value)]"
+        >{{ option.value }}</Radio
+      >
+    </div>
+    <div v-if="!required" class="margin-top-1">
+      <a @click=";[touch(), updateValue(null, value)]">clear value</a>
+    </div>
+    <div v-show="touched && !valid" class="error">
+      {{ error }}
+    </div>
+  </div>
+</template>
+
+<script>
+import rowEditField from '@baserow/modules/database/mixins/rowEditField'
+import singleSelectField from '@baserow/modules/database/mixins/singleSelectField'
+
+export default {
+  name: 'FormViewSingleSelectRadios',
+  mixins: [rowEditField, singleSelectField],
+}
+</script>
