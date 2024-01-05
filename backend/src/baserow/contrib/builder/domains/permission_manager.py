9a63c007eed5c0aef70b6a6e05b5a6457331adee
Jérémie Pardou <jeremie@baserow.io>
2024-01-05 10:56:20 +0000

    Fix permission denied on public website action dispatch

--- backend/src/baserow/contrib/builder/domains/permission_manager.py
+++ backend/src/baserow/contrib/builder/domains/permission_manager.py
@@ -6,11 +6,14 @@ from baserow.contrib.builder.data_sources.operations import (
 )
 from baserow.contrib.builder.elements.operations import ListElementsPageOperationType
 from baserow.contrib.builder.models import Builder
+from baserow.contrib.builder.workflow_actions.operations import (
+    DispatchBuilderWorkflowActionOperationType,
+    ListBuilderWorkflowActionsPageOperationType,
+)
 from baserow.core.operations import ReadApplicationOperationType
 from baserow.core.registries import PermissionManagerType, operation_type_registry
 from baserow.core.subjects import AnonymousUserSubjectType, UserSubjectType
 
-from ..workflow_actions.operations import ListBuilderWorkflowActionsPageOperationType
 from .models import Domain
 
 User = get_user_model()
@@ -23,12 +26,18 @@ class AllowPublicBuilderManagerType(PermissionManagerType):
 
     type = "allow_public_builder"
     supported_actor_types = [UserSubjectType.type, AnonymousUserSubjectType.type]
+
+    # Public elements and public data sources
     page_level_operations = [
         ListElementsPageOperationType.type,
         ListDataSourcesPageOperationType.type,
         ListBuilderWorkflowActionsPageOperationType.type,
     ]
-    sub_page_level_operations = [DispatchDataSourceOperationType.type]
+    # Data sources or Action dispatch
+    sub_page_level_operations = [
+        DispatchDataSourceOperationType.type,
+        DispatchBuilderWorkflowActionOperationType.type,
+    ]
     application_level_operations = [ReadApplicationOperationType.type]
 
     def check_multiple_permissions(self, checks, workspace=None, include_trash=False):
@@ -36,14 +45,10 @@ class AllowPublicBuilderManagerType(PermissionManagerType):
 
         for check in checks:
             operation_type = operation_type_registry.get(check.operation_name)
-            # Public elements and public data sources
             if operation_type.type in self.page_level_operations:
                 builder = check.context.builder
-
-            # Data sources dispatch
             elif operation_type.type in self.sub_page_level_operations:
                 builder = check.context.page.builder
-            # Builder
             elif (
                 operation_type.type in self.application_level_operations
                 and isinstance(check.context.specific, Builder)
