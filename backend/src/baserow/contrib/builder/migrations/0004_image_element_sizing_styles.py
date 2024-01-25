49d73f834cd5ebb66bb5cb38605ae9cd743c08ef
Peter Evans <peter@baserow.io>
2024-01-25 07:46:57 +0000

    Fix builder squashed migrations 0003-0004

--- backend/src/baserow/contrib/builder/migrations/0004_image_element_sizing_styles.py
+++ backend/src/baserow/contrib/builder/migrations/0004_image_element_sizing_styles.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("builder", "0002_rename_paragraphelement_textelement"),
+        ("builder", "0003_heading_text_alignment"),
     ]
 
     operations = [
