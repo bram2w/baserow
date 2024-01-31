124fb9aa23289c4adffada9ec873f130024c78f1
Afonso Silva <afonso@baserow.io>
2024-01-31 16:22:33 +0000

    Resolve "Add an option to enter Markdown for the paragraph element"

---
+++ web-frontend/modules/builder/components/theme/ThemeProvider.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :style="style">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ThemeProvider',
+  inject: ['builder'],
+  computed: {
+    style() {
+      const colors = {
+        '--primary-color': this.builder.theme.primary_color,
+        '--secondary-color': this.builder.theme.secondary_color,
+      }
+      const headings = Array.from([1, 2, 3, 4, 5, 6]).reduce(
+        (headings, level) => ({
+          [`--heading-h${level}--font-size`]: `${
+            this.builder.theme[`heading_${level}_font_size`]
+          }px`,
+          [`--heading-h${level}--color`]:
+            this.builder.theme[`heading_${level}_color`],
+          ...headings,
+        }),
+        {}
+      )
+      return { ...colors, ...headings }
+    },
+  },
+}
+</script>
