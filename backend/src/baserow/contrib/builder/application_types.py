6683b68593af90b357867e33a0289a97b9285e3a
Tsering Paljor <20007514-paljort@users.noreply.gitlab.com>
2024-03-18 11:10:18 +0000

    AB Fix Data Source error

--- backend/src/baserow/contrib/builder/application_types.py
+++ backend/src/baserow/contrib/builder/application_types.py
@@ -43,6 +43,7 @@ class BuilderApplicationType(ApplicationType):
     serializer_field_names = [
         "name",
         "pages",
+        "integrations",
         "user_sources",
         "theme",
     ]
