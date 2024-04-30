b1a17fe5895d1f351da1a153a92148bb4165bcf1
Peter Evans <peter@baserow.io>
2024-04-30 05:44:30 +0000

    Resolve "The PublishDomainJob can be run with a deleted Domain."

--- backend/src/baserow/contrib/builder/domains/job_types.py
+++ backend/src/baserow/contrib/builder/domains/job_types.py
@@ -1,8 +1,10 @@
 from baserow.api.errors import ERROR_USER_NOT_IN_GROUP
+from baserow.contrib.builder.api.domains.errors import ERROR_DOMAIN_DOES_NOT_EXIST
 from baserow.core.exceptions import UserNotInWorkspace
 from baserow.core.jobs.registries import JobType
 from baserow.core.registries import application_type_registry
 
+from .exceptions import DomainDoesNotExist
 from .models import PublishDomainJob
 
 
@@ -17,9 +19,16 @@ class PublishDomainJobType(JobType):
 
     api_exceptions_map = {
         UserNotInWorkspace: ERROR_USER_NOT_IN_GROUP,
+        DomainDoesNotExist: ERROR_DOMAIN_DOES_NOT_EXIST,
     }
 
     def transaction_atomic_context(self, job: PublishDomainJob):
+        # It's possible for the Domain to be deleted prior to the execution of
+        # this task (e.g. the export worker queue is down, and brought up again).
+        if not job.domain:
+            raise DomainDoesNotExist(
+                "The Domain has been deleted prior to the execution of this task."
+            )
         application_type = application_type_registry.get("builder")
         return application_type.export_safe_transaction_context(job.domain.builder)
 
