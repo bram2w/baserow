977ff1d0439e1203fd866f75e08a713ccc6badd8
Bram Wiepjes <bramw@protonmail.com>
2024-05-30 12:25:46 +0200

    Prepare for 1.25.1 release

--- backend/src/baserow/config/settings/base.py
+++ backend/src/baserow/config/settings/base.py
@@ -485,7 +485,7 @@ SPECTACULAR_SETTINGS = {
         "name": "MIT",
         "url": "https://gitlab.com/baserow/baserow/-/blob/master/LICENSE",
     },
-    "VERSION": "1.25.0",
+    "VERSION": "1.25.1",
     "SERVE_INCLUDE_SCHEMA": False,
     "TAGS": [
         {"name": "Settings"},
