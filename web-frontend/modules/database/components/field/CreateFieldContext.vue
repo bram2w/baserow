8baca67c379b4f347a28b5d5ad37efbc44e63b07
Cezary Statkiewicz <cezary@baserow.io>
2024-06-11 09:37:51 +0000

    fix #2715 - form field: add description button won't capture keyboard events from other form fields

--- web-frontend/modules/database/components/field/CreateFieldContext.vue
+++ web-frontend/modules/database/components/field/CreateFieldContext.vue
@@ -26,6 +26,8 @@
           <ButtonText
             v-if="!showDescription"
             ref="showDescription"
+            tag="a"
+            class="button-text--no-underline"
             icon="iconoir iconoir-plus"
             type="secondary"
             @click="showDescriptionField"
