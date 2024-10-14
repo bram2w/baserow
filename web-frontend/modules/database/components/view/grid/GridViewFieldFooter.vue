891ba0fec7a4bd4da93859a4a4d6e702ec94c7e9
Bram Wiepjes <bramw@protonmail.com>
2024-10-14 06:59:18 +0000

    Show summarize label in grid view footer

--- web-frontend/modules/database/components/view/grid/GridViewFieldFooter.vue
+++ web-frontend/modules/database/components/view/grid/GridViewFieldFooter.vue
@@ -22,7 +22,18 @@
       :aggregation-type="viewAggregationType"
       :value="value"
     />
-    <Context :ref="`fieldContext`">
+    <div
+      v-else-if="userCanMakeAggregations"
+      class="grid-view-aggregation__empty"
+      :class="{
+        'grid-view-aggregation__empty--active':
+          $refs.fieldContext && $refs.fieldContext.isOpen(),
+      }"
+    >
+      <i class="grid-view-aggregation__empty-icon iconoir-plus"></i>
+      {{ $t('common.summarize') }}
+    </div>
+    <Context ref="fieldContext">
       <ul class="select__items">
         <li
           class="select__item select__item--no-options"
