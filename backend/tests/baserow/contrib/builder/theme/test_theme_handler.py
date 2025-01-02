80c93f77a4233a23591af4fdce4e51e8ed812902
Davide Silvestri <davide@baserow.io>
2025-01-02 08:10:21 +0000

    Resolve "Cannot export applications with `PageThemeConfigBlockType`"

--- backend/tests/baserow/contrib/builder/theme/test_theme_handler.py
+++ backend/tests/baserow/contrib/builder/theme/test_theme_handler.py
@@ -1,7 +1,14 @@
+from io import BytesIO
+
+from django.test import override_settings
+
 import pytest
 
 from baserow.contrib.builder.theme.handler import ThemeHandler
 from baserow.contrib.builder.theme.registries import theme_config_block_registry
+from baserow.core.storage import ExportZipFile, get_default_storage
+from baserow.core.user_files.handler import UserFileHandler
+from baserow.test_utils.helpers import AnyStr
 
 
 @pytest.mark.django_db
@@ -29,3 +36,27 @@ def test_update_theme(data_fixture):
     assert builder.typographythemeconfigblock.heading_3_font_size == 16
     assert builder.typographythemeconfigblock.heading_3_text_color == "#070810ff"
     assert builder.buttonthemeconfigblock.button_background_color == "#ccddcc"
+
+
+# Gitlab issue #3320
+@pytest.mark.django_db
+@override_settings(DEFAULT_FILE_STORAGE="django.core.files.storage.InMemoryStorage")
+def test_theme_with_page_background_file_can_be_exported(data_fixture):
+    user = data_fixture.create_user()
+    builder = data_fixture.create_builder_application()
+    handler = ThemeHandler()
+    files_zip = ExportZipFile()
+
+    storage = get_default_storage()
+    page_background_file = UserFileHandler().upload_user_file(
+        user, "a.png", BytesIO(), storage=storage
+    )
+    builder = handler.update_theme(builder, page_background_file=page_background_file)
+    serialized_theme = ThemeHandler().export_theme(
+        builder, files_zip=files_zip, storage=storage
+    )
+
+    assert serialized_theme["page_background_file_id"] == {
+        "name": AnyStr(),
+        "original_name": "a.png",
+    }
