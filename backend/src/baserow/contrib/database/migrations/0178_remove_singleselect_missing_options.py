8d447126ba80a8059db097484e3d513bb7fc38e9
Bram Wiepjes <bramw@protonmail.com>
2025-02-04 13:00:23 +0000

    Fix single select missing option migration

--- backend/src/baserow/contrib/database/migrations/0178_remove_singleselect_missing_options.py
+++ backend/src/baserow/contrib/database/migrations/0178_remove_singleselect_missing_options.py
@@ -1,30 +1,36 @@
-from django.db import connection, migrations, transaction
+from django.db import ProgrammingError, connection, migrations, transaction
 
 from psycopg2 import sql
 
 
 def forward(apps, schema_editor):
-    batch_size = 50
     SelectOption = apps.get_model("database", "SelectOption")
     SingleSelectField = apps.get_model("database", "SingleSelectField")
 
     option_table_name = SelectOption.objects.model._meta.db_table
-    total = SingleSelectField.objects.count()
 
-    for start in range(0, total, batch_size):
-        with transaction.atomic():
-            for field in SingleSelectField.objects.all()[start : start + batch_size]:
+    for field in SingleSelectField.objects.all().order_by("id").iterator(chunk_size=50):
+        table_name = f"database_table_{field.table_id}"
+        try:
+            with transaction.atomic():  # Start a new transaction for each query
                 with connection.cursor() as cursor:
                     query = sql.SQL(
                         "UPDATE {table} SET {select_column} = NULL "
                         "WHERE {select_column} NOT IN "
                         "(SELECT id FROM {option_table})"
                     ).format(
-                        table=sql.Identifier(f"database_table_{field.table.id}"),
+                        table=sql.Identifier(table_name),
                         select_column=sql.Identifier(f"field_{field.id}"),
                         option_table=sql.Identifier(option_table_name)
                     )
                     cursor.execute(query)
+        except ProgrammingError as e:
+            error_string = str(e)
+            if f'relation "{table_name}" does not exist' in error_string:
+                print(f"Ignoring because table {table_name} does not exist in "
+                      f"PostgreSQL.")
+                continue
+            raise e
 
 
 class Migration(migrations.Migration):
