119e0d11b0ed43d6154cf520a15da07465c96ebb
Bram Wiepjes <bramw@protonmail.com>
2025-02-05 00:16:46 +0100

    Improve single select field missing options removal performance

--- backend/src/baserow/contrib/database/migrations/0178_remove_singleselect_missing_options.py
+++ backend/src/baserow/contrib/database/migrations/0178_remove_singleselect_missing_options.py
@@ -11,21 +11,54 @@ def forward(apps, schema_editor):
 
     for field in SingleSelectField.objects.all().order_by("id").iterator(chunk_size=50):
         table_name = f"database_table_{field.table_id}"
+        select_column = f"field_{field.id}"
         try:
             with transaction.atomic():  # Start a new transaction for each query
                 with connection.cursor() as cursor:
+                    # First, check if there are records to update, so that we don't
+                    # have to start and update query if not really needed.
+                    pre_check_query = sql.SQL("""
+                        SELECT EXISTS (
+                            SELECT 1 FROM {table}
+                            WHERE {select_column} IS NOT NULL
+                            AND NOT EXISTS (
+                                SELECT 1 FROM {option_table} opt
+                                WHERE opt.id = {table}.{select_column}
+                            )
+                            LIMIT 1
+                        )
+                    """).format(
+                        table=sql.Identifier(table_name),
+                        select_column=sql.Identifier(select_column),
+                        option_table=sql.Identifier(option_table_name)
+                    )
+
+                    cursor.execute(pre_check_query)
+                    should_update = cursor.fetchone()[0]
+
+                    if not should_update:
+                        print(
+                            f"Skipping update for {table_name}.{select_column} because "
+                            f"no changes are needed."
+                        )
+                        continue
+
                     query = sql.SQL("""
-                        UPDATE {table} SET {select_column} = NULL
-                        WHERE NOT EXISTS (
+                        UPDATE {table}
+                        SET {select_column} = NULL
+                        WHERE {select_column} IS NOT NULL
+                        AND NOT EXISTS (
                             SELECT 1 FROM {option_table} opt
                             WHERE opt.id = {table}.{select_column}
                         )
                     """).format(
                         table=sql.Identifier(table_name),
-                        select_column=sql.Identifier(f"field_{field.id}"),
+                        select_column=sql.Identifier(select_column),
                         option_table=sql.Identifier(option_table_name)
                     )
                     cursor.execute(query)
+
+                    print(f"Updated {table_name}.{select_column}")
         except ProgrammingError as e:
             error_string = str(e)
             if f'relation "{table_name}" does not exist' in error_string:
