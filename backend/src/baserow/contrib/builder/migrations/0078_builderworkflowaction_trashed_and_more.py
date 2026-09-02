fc0c4086ae4607540a98945a3ed33608aafbe0d8
peter_baserow <peter@baserow.io>
2026-09-02 10:53:27 +0100

    Post rebase tweaks again - tweak migrations, fix trash support in the new database workflow actions.

--- backend/src/baserow/contrib/builder/migrations/0078_builderworkflowaction_trashed_and_more.py
+++ backend/src/baserow/contrib/builder/migrations/0078_builderworkflowaction_trashed_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('builder', '0076_iframeelement_allow_same_origin'),
+        ('builder', '0077_builder_breakpoints'),
     ]
 
     operations = [
